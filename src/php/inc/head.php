d03d8c971976f1eeb9c1bbbd21507b9ee9f2a1ca
Kerogs <43384553+kerogs@users.noreply.github.com>
2024-05-02 01:14:10 +0200

    Add all icon for the app

--- src/php/inc/head.php
+++ src/php/inc/head.php
@@ -1,3 +1,3 @@
-<link rel="shortcut icon" href="./src/img/favicon.png" type="image/x-icon">
+<link rel="shortcut icon" href="./src/img/web/favicon.ico" type="image/x-icon">
 <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 <link rel="manifest" href="manifest.json">
